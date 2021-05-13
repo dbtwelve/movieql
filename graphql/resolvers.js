5197b752c9f9523088f58516c319edd3415e1b52
dbtwelve <dbtwelve12@gmail.com>
2021-05-13 22:47:07 +0900

    change from fetch to axios

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -2,9 +2,10 @@ import { getMovies } from "./db";
 
 const resolvers = {
     Query: {
-        movies: (_, {limit, rating}) => getMovies(limit, rating)
-    },
-
-}
+      movies: (_, { rating, limit }) => getMovies(limit, rating),
+      movie: (_, { id }) => getMovie(id),
+      suggestions: (_, { id }) => getSuggestions(id)
+    }
+  };
 
 export default resolvers

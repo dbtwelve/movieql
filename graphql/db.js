e82ca5d3c3b33469ed8d3d0a924dc8aa72913b5e
dbtwelve <dbtwelve12@gmail.com>
2021-05-13 22:40:50 +0900

    yts api added

--- graphql/db.js
+++ graphql/db.js
@@ -3,53 +3,15 @@ import fetch from "node-fetch";
 const API_URL = "https://yts.mx/api/v2/list_movies.json?quality=3D"
 
 export const getMovies = (limit, rating) => {
-    fetch(`${API_URL}`)
+    let REQUEST_URL = API_URL;
+    if (limit > 0){
+        REQUEST_URL += `&limit=${limit}`;
+    }
+    if (rating > 0){
+        
+        REQUEST_URL += `&minimum_rating=${rating}`;
+    }
+    return fetch(`${REQUEST_URL}`)
     .then(res => res.json())
     .then(json => json.data.movies);
-}
-
-export let movies = [
-    {
-        id: 0,
-        name: "super",
-        score: 1,
-        
-    },
-    {
-        id: 1,
-        name: "spider",
-        score: 99,
-        
-    },
-    {
-        id: 2,
-        name: "iron",
-        score: 2,
-        
-    },
-];
-
-export const getById = id => {
-    const filteredMovies = movies.filter(movie => id === movie.id);
-    return filteredMovies[0];
-};
-
-export const deleteMovie = id => {
-    const cleanedMovies = movies.filter(movie => movie.id !== id);
-    if (movies.length > cleanedMovies.length) {
-        movies = cleanedMovies;
-        return true;
-    } else {
-        return false;
-    }
-};
-
-export const addMovie = (name, score) => {
-    const newMovie = {
-        id: `${movies.length + 1}`,
-        name,
-        score
-    };
-    movies.push(newMovie);
-    return newMovie;
 }
